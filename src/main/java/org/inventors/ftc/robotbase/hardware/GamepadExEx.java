566cab51ed277813e53c4647dca2efcb59f49c26
Angelos Theodoridis <72686781+Agtheo09@users.noreply.github.com>
2024-06-18 23:28:00 +0300

    Rumble Changes on GamepadExEx

--- src/main/java/org/inventors/ftc/robotbase/hardware/GamepadExEx.java
+++ src/main/java/org/inventors/ftc/robotbase/hardware/GamepadExEx.java
@@ -1,23 +1,39 @@
 package org.inventors.ftc.robotbase.hardware;
 
+
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class GamepadExEx extends GamepadEx {
-    Gamepad.RumbleEffect rumbleEffect = new Gamepad.RumbleEffect.Builder()
-            .addStep(1.0, 1.0, 600)
-            .build();
 
     public GamepadExEx(Gamepad gp) {
         super(gp);
     }
 
-    public GamepadExEx(Gamepad gp, Gamepad.RumbleEffect effect) {
-        super(gp);
-        this.rumbleEffect = effect;
+    public void rumble() {
+        Gamepad.RumbleEffect rumbleEffect = new Gamepad.RumbleEffect.Builder()
+                .addStep(1.0, 1.0, 600)
+                .build();
+
+        gamepad.runRumbleEffect(rumbleEffect);
     }
 
-    public void rumble() {
+    public void rumble(double duration) {
+        Gamepad.RumbleEffect rumbleEffect = new Gamepad.RumbleEffect.Builder()
+                .addStep(1.0, 1.0, (int)duration*1000)
+                .build();
+
+        gamepad.runRumbleEffect(rumbleEffect);
+    }
+
+    public void rumble(double duration, double stress) {
+        Gamepad.RumbleEffect rumbleEffect = new Gamepad.RumbleEffect.Builder()
+                .addStep(stress, stress, (int)duration*1000)
+                .build();
+
         gamepad.runRumbleEffect(rumbleEffect);
     }
 }
+
+
+
