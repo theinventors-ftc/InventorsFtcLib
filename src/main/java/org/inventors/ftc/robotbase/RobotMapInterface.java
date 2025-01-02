86fb0e3f7e5963ccf84c17e44858fe5c3dfbec05
Agtheo09 <agtheo09@gmail.com>
2025-01-02 11:05:33 +0200

    ++Interface

---
+++ src/main/java/org/inventors/ftc/robotbase/RobotMapInterface.java
@@ -0,0 +1,29 @@
+package org.inventors.ftc.robotbase;
+
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.inventors.ftc.robotbase.hardware.Camera;
+import org.inventors.ftc.robotbase.hardware.GamepadExEx;
+import org.inventors.ftc.robotbase.hardware.MotorExEx;
+import org.inventors.ftc.robotbase.hardware.RevIMU;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+public interface RobotMapInterface {
+    // ----------------------------------------- Telemetry -------------------------------------- //
+    Telemetry getTelemetry();
+
+    // ------------------------------------ Drivetrain Motors ----------------------------------- //
+    MotorExEx getFrontLeftMotor();
+    MotorExEx getFrontRightMotor();
+    MotorExEx getRearLeftMotor();
+    MotorExEx getRearRightMotor();
+
+    // ------------------------------------------ Sensors --------------------------------------- //
+    OpenCvWebcam getCamera();
+    IMU getIMU();
+
+    // ------------------------------------------ Gamepads -------------------------------------- //
+    GamepadExEx getDriverOp();
+    GamepadExEx getToolOp();
+}
